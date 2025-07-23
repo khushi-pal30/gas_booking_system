25caa4b7a0a03759a5abe486ad426738ae7d24bc
khushi-pal30 <palkhushi163@gmail.com>
2025-07-23 22:56:15 +0530

    Push requirements.txt and other files properly

--- gas_booking/settings.py
+++ gas_booking/settings.py
@@ -122,3 +122,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
